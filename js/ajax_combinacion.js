03d2fb754994b8e68ca19d30406bb62924d7f73b
Francis Tocino <francistocino@hotmail.com>
2020-10-22 13:16:40 +0200

    arreglo el tema de las gráficas

--- js/ajax_combinacion.js
+++ js/ajax_combinacion.js
@@ -1,75 +1,73 @@
-
-function llamada_combinacion_primitiva(timeline_valor){
-		var parametros = {"timeline":timeline_valor};
-		$.ajax({
-			url : '../_tunedlotto/php/calculo_combinacion/calculo_primitiva.php',
-			type: 'post',
-			data: parametros,
-			beforeSend: function () {
-                        $("#resultado_pr").html("Procesando, espere por favor...");
-        	},
-            success:  function (response) {
-                        $("#resultado_pr").html(response);
-            }
-		});
-
-};
-
-
-function llamada_combinacion_bonoloto(timeline_valor){
-		var parametros = {"timeline":timeline_valor};
-		$.ajax({
-			url : '../_tunedlotto/php/calculo_combinacion/calculo_bonoloto.php',
-			type: 'post',
-			data: parametros,
-			beforeSend: function () {
-						//alert('BEFORE');
-                        $("#resultado_bn").html("Procesando, espere por favor...");
-        	},
-            success:  function (response) {
-            			//alert('RESPONSSE');
-                        $("#resultado_bn").html(response);
-            }
-		});
+function llamada_combinacion_primitiva(timeline_valor) {
+    var parametros = { "timeline": timeline_valor };
+    $.ajax({
+        url: '../_tunedlotto/php/calculo_combinacion/calculo_primitiva.php',
+        type: 'post',
+        data: parametros,
+        beforeSend: function() {
+            $("#resultado_pr").html("Procesando, espere por favor...");
+        },
+        success: function(response) {
+            $("#resultado_pr").html(response);
+        }
+    });
 
 };
 
 
-function llamada_combinacion_euromillones(timeline_valor){
-		var parametros = {"timeline":timeline_valor};
-		$.ajax({
-			url : '../_tunedlotto/php/calculo_combinacion/calculo_euromillones.php',
-			type: 'post',
-			data: parametros,
-			beforeSend: function () {
-						//alert('BEFORE');
-                        $("#resultado_eu").html("Procesando, espere por favor...");
-        	},
-            success:  function (response) {
-            			//alert('RESPONSSE');
-                        $("#resultado_eu").html(response);
-            }
-		});
+function llamada_combinacion_bonoloto(timeline_valor) {
+    var parametros = { "timeline": timeline_valor };
+    $.ajax({
+        url: '../_tunedlotto/php/calculo_combinacion/calculo_bonoloto.php',
+        type: 'post',
+        data: parametros,
+        beforeSend: function() {
+            //alert('BEFORE');
+            $("#resultado_bn").html("Procesando, espere por favor...");
+        },
+        success: function(response) {
+            //alert('RESPONSSE');
+            $("#resultado_bn").html(response);
+        }
+    });
 
 };
 
 
-function llamada_combinacion_elgordo(timeline_valor){
-
-		var parametros = {"timeline":timeline_valor};
-		$.ajax({
-			url : '../_tunedlotto/php/calculo_combinacion/calculo_elgordo.php',
-			type: 'post',
-			data: parametros,
-			beforeSend: function () {
-						//alert('BEFORE');
-                        $("#resultado_eg").html("Procesando, espere por favor.");
-        	},
-            success:  function (response) {
-            			//alert(response);
-                        $("#resultado_eg").html(response);
-            }
-		});
+function llamada_combinacion_euromillones(timeline_valor) {
+    var parametros = { "timeline": timeline_valor };
+    $.ajax({
+        url: '../_tunedlotto/php/calculo_combinacion/calculo_euromillones.php',
+        type: 'post',
+        data: parametros,
+        beforeSend: function() {
+            //alert('BEFORE');
+            $("#resultado_eu").html("Procesando, espere por favor...");
+        },
+        success: function(response) {
+            //alert('RESPONSSE');
+            $("#resultado_eu").html(response);
+        }
+    });
 
 };
 
+
+function llamada_combinacion_elgordo(timeline_valor) {
+
+    var parametros = { "timeline": timeline_valor };
+    $.ajax({
+        url: '../_tunedlotto/php/calculo_combinacion/calculo_elgordo.php',
+        type: 'post',
+        data: parametros,
+        beforeSend: function() {
+            //alert('BEFORE');
+            $("#resultado_eg").html("Procesando, espere por favor.");
+        },
+        success: function(response) {
+            //alert(response);
+            $("#resultado_eg").html(response);
+        }
+    });
+
+};
